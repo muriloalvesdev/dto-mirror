ccf3831f1d90ad7fd7c7ce137e2b274032e40df5
Murilo Alves <murilohenrique.ti@outlook.com.br>
2024-02-21 11:15:24 -0300

    format

--- src/main/java/dto/core/aspect/ExcludeFieldsAspect.java
+++ src/main/java/dto/core/aspect/ExcludeFieldsAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
 import java.lang.reflect.Field;
 
 @Aspect
@@ -19,14 +20,15 @@ public class ExcludeFieldsAspect {
      * Pointcut definition for methods annotated with RestController or Controller annotations.
      */
     @Pointcut("@within(org.springframework.web.bind.annotation.RestController) || @within(org.springframework.stereotype.Controller)")
-    public void controllerPointcut() {}
+    public void controllerPointcut() {
+    }
 
     /**
      * Advice to execute after a method annotated with RestController or Controller annotations returns.
      * This advice will exclude specified fields from the response.
      *
      * @param excludeFields The ExcludeFields annotation specifying fields to be excluded.
-     * @param result The result object returned from the method.
+     * @param result        The result object returned from the method.
      * @throws IllegalAccessException If an illegal access exception occurs during field modification.
      */
     @AfterReturning(pointcut = "controllerPointcut() && execution(public * *(..)) && @annotation(excludeFields)", returning = "result")
@@ -45,7 +47,7 @@ public class ExcludeFieldsAspect {
     /**
      * Determines whether a given field should be excluded based on the ExcludeFields annotation.
      *
-     * @param field The field to be checked for exclusion.
+     * @param field         The field to be checked for exclusion.
      * @param excludeFields The ExcludeFields annotation specifying fields to be excluded.
      * @return True if the field should be excluded, false otherwise.
      */
