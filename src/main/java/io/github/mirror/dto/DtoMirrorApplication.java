aca383dc05b27a8e9c70ef301c21519951b41894
Murilo Alves <murilohenrique.ti@outlook.com.br>
2024-11-17 17:05:15 -0300

    javadoc and improvings

---
+++ src/main/java/io/github/mirror/dto/DtoMirrorApplication.java
@@ -0,0 +1,11 @@
+package io.github.mirror.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoMirrorApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DtoMirrorApplication.class, args);
+  }
+}
--- src/main/java/io/github/mirror/dto/aspect/DTOMirrorAspect.java
+++ src/main/java/io/github/mirror/dto/aspect/DTOMirrorAspect.java
@@ -1,5 +1,6 @@
 package io.github.mirror.dto.aspect;
 
+import io.github.mirror.dto.annotation.DTOProcessor;
 import io.github.mirror.dto.annotation.ExcludeFields;
 import io.github.mirror.dto.annotation.MapField;
 import java.lang.reflect.Field;
@@ -9,52 +10,83 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.expression.BeanFactoryResolver;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
+/**
+ * Aspect responsible for processing DTO transformations based on annotations such as {@link
+ * MapField} and {@link ExcludeFields}, applied only to classes annotated with {@link DTOProcessor}.
+ *
+ * <p><b>Author:</b> muriloalvesdev
+ */
 @Aspect
 @Component
 public class DTOMirrorAspect {
 
-  @Autowired
-  private ApplicationContext applicationContext;
+  @Autowired private ApplicationContext applicationContext;
 
+  /** Pointcut that matches any method within classes annotated with {@link DTOProcessor}. */
+  @Pointcut("@within(io.github.mirror.dto.annotation.DTOProcessor)")
+  public void dtoProcessorAnnotatedClass() {}
+
+  /**
+   * Pointcut that matches any public method within classes annotated with {@link RestController} or
+   * {@link Controller}.
+   */
   @Pointcut(
-          "@within(org.springframework.web.bind.annotation.RestController) || @within(org.springframework.stereotype.Controller)")
+      "@within(org.springframework.web.bind.annotation.RestController) || @within(org.springframework.stereotype.Controller)")
   public void controllerPointcut() {}
 
+  /**
+   * Executes only for public methods in classes annotated with both {@link RestController} (or
+   * {@link Controller}) and {@link DTOProcessor}, and applies transformations or exclusions as
+   * specified in the annotations.
+   */
   @AfterReturning(
-          pointcut = "controllerPointcut() && execution(public * *(..)) && @annotation(excludeFields)",
-          returning = "result")
+      pointcut =
+          "dtoProcessorAnnotatedClass() && controllerPointcut() && execution(public * *(..)) && @annotation(excludeFields)",
+      returning = "result")
   public void apply(ExcludeFields excludeFields, Object result) throws IllegalAccessException {
     if (result != null) {
       Class<?> resultClass = result.getClass();
       SpelExpressionParser parser = new SpelExpressionParser();
 
-      StandardEvaluationContext context = new StandardEvaluationContext(result); // Define o objeto raiz como `result`
+      StandardEvaluationContext context =
+          new StandardEvaluationContext(result); // Define the root object as `result`
       context.setBeanResolver(new BeanFactoryResolver(this.applicationContext));
 
       for (Field field : resultClass.getDeclaredFields()) {
         field.setAccessible(true);
+        processMapFields(result, field, context, parser);
+        processExcludeFields(excludeFields, result, field);
+      }
+    }
+  }
 
-        if (shouldExcludeField(field, excludeFields)) {
-          exclude(result, field);
-          continue;
-        }
+  private void processExcludeFields(ExcludeFields excludeFields, Object result, Field field)
+      throws IllegalAccessException {
+    if (shouldExcludeField(field, excludeFields)) {
+      exclude(result, field);
+    }
+  }
 
-        var mapFieldAnnotation = field.getAnnotation(MapField.class);
-        if (mapFieldAnnotation != null) {
-          String expression = mapFieldAnnotation.expression();
-          try {
-            context.setVariable(field.getName(), field.get(result));
-            Object transformedValue = parser.parseExpression(expression).getValue(context);
+  private void processMapFields(
+      Object result, Field field, StandardEvaluationContext context, SpelExpressionParser parser)
+      throws IllegalAccessException {
+    var mapFieldAnnotation = field.getAnnotation(MapField.class);
+    if (mapFieldAnnotation != null) {
+      String expression = mapFieldAnnotation.expression();
+      try {
+        context.setRootObject(result);
 
-            field.set(result, transformedValue);
-          } catch (Exception e) {
-            throw new IllegalAccessException("Error processing MapField expression: " + expression + " - " + e.getMessage());
-          }
-        }
+        Object transformedValue = parser.parseExpression(expression).getValue(context);
+
+        field.setAccessible(true);
+        field.set(result, transformedValue);
+      } catch (Exception e) {
+        throw new IllegalAccessException(
+            "Error processing MapField expression: " + expression + " - " + e.getMessage());
       }
     }
   }
